e9bdfde5414adce9e485a4138b2669ce832baeea
Mohit Mudgil <mohittt0011@gmail.com>
2025-12-19 13:15:53 +0530

    Export api instance for external usage

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
-
-export default api;
