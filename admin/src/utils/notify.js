678bdcd2aadd4a1a86210190aa85042637706a66
Mohit Mudgil <mohittt0011@gmail.com>
2025-12-21 13:19:08 +0530

    Update notify.js

--- admin/src/utils/notify.js
+++ admin/src/utils/notify.js
@@ -1,6 +1,5 @@
-// admin/src/utils/notify.js
-import { toast } from "react-toastify";
-
-export const notifySuccess = (msg) => toast.success(msg, { position: "top-right" });
-export const notifyError = (msg) => toast.error(msg, { position: "top-right" });
-export const notifyInfo = (msg) => toast.info(msg, { position: "top-right" });
+export function vibrate() {
+  try {
+    navigator.vibrate?.([200, 100, 200]);
+  } catch {}
+}
