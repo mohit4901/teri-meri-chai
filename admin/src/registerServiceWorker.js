68ce928dde7ac6129e0abe50c9838d35e7b7df0b
Mohit Mudgil <mohittt0011@gmail.com>
2025-12-28 11:57:10 +0530

    Create registerServiceWorker.js

---
+++ admin/src/registerServiceWorker.js
@@ -0,0 +1,8 @@
+export function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("✅ Service Worker registered"))
+      .catch(err => console.error("❌ SW registration failed", err));
+  }
+}
